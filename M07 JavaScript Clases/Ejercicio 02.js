5d2dd8ca716ea0ba026260e9e30d40685e069f2c
stevenn77 <jonathan4562009@icloud.com>
2023-01-19 20:18:39 -0500

    TAREA TERMINADA

--- M07 JavaScript Clases/Ejercicio 02.js
+++ M07 JavaScript Clases/Ejercicio 02.js
@@ -6,6 +6,22 @@ class Persona {
    // Debe tener las propiedades: "nombre", "apellido", "edad" y "domicilio".
    // Debe tener un método llamado "detalle" que nos devuelva un objeto con las propiedades de la persona y
    // sus valores.
+     constructor (nombre,apellido,edad,domicilio){
+               this.nombre = nombre
+               this.apellido = apellido
+               this.edad = edad
+               this.domicilio = domicilio
+     }
+            detalle (nombre,apellido,edad,domicilio){         // ESTE ES EL OBJETO CREADO
+               return {
+                  nombre: this.nombre,
+                  apellido: this.apellido,
+                  edad: this.edad,
+                  domicilio: this.domicilio
+
+               }
+            } 
+            
 }
 
 function crearInstanciaPersona(nombre, apellido, edad, domicilio) {
@@ -13,6 +29,9 @@ function crearInstanciaPersona(nombre, apellido, edad, domicilio) {
    // Recibirás las propiedades por parámetro.
    // Retornar la instancia creada.
    // Tu código:
+     var persona = new Persona(nombre,apellido,edad,domicilio)
+     return persona
+
 }
 
 function agregarMetodo() {
@@ -20,6 +39,9 @@ function agregarMetodo() {
    // Este método toma la propiedad "nombre" y "edad", y devuelve el string:
    // Ejemplo: "Juan, 22 años".
    // Tu código:
+   Persona.prototype.datos = function(){
+      return (this.nombre + ", " + this.edad + " años")
+   }
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
